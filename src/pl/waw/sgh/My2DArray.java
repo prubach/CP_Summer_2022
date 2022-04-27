6061a0efcc144ca9bc9979b8c034781ae84b8494
prubach <pawel.rubach@sgh.waw.pl>
2022-04-27 15:03:26 +0200

    Sum arrays

--- src/pl/waw/sgh/My2DArray.java
+++ src/pl/waw/sgh/My2DArray.java
@@ -1,5 +1,7 @@
 package pl.waw.sgh;
 
+import java.util.Arrays;
+
 public class My2DArray {
     public static void main(String[] args) {
         Double[][] my2Darr = new Double[3][4];
@@ -10,12 +12,31 @@ public class My2DArray {
         my2Darr[1][3] = 60.0;
         my2Darr[2] = new Double[] { 3.0, 5.9, 9.4, 8.0};
 
+        Double[] sumRows = new Double[] { 0.0, 0.0, 0.0};
+        Double[] sumCols = new Double[] { 0.0, 0.0, 0.0, 0.0};
+
         for (int i=0;i<my2Darr.length;i++) {
             // my2Darr[i] - array = row
             for (int j=0;j<my2Darr[i].length;j++) {
                 System.out.println("El at [" + i + ", " + j + "]: " + my2Darr[i][j]);
+                sumRows[i] += my2Darr[i][j];
+                sumCols[j] += my2Darr[i][j];
             }
         }
+        System.out.println(Arrays.toString(sumRows));
+        System.out.println(Arrays.toString(sumCols));
+
+        Double allRows = 0.0;
+        for (Double el : sumRows) {
+            allRows +=el;
+        }
+        System.out.println("All rows: " + allRows);
+
+        Double allCols = 0.0;
+        for (Double el : sumCols) {
+            allCols +=el;
+        }
+        System.out.println("All cols: " + allCols);
 
     }
 }
