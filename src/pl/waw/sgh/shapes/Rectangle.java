c4000b5134a8b0af5ef089880589d1e307ee7917
prubach <pawel.rubach@sgh.waw.pl>
2022-05-18 14:10:17 +0200

    Comparison of objects, toString, equals

--- src/pl/waw/sgh/shapes/Rectangle.java
+++ src/pl/waw/sgh/shapes/Rectangle.java
@@ -1,5 +1,7 @@
 package pl.waw.sgh.shapes;
 
+import java.util.Objects;
+
 public class Rectangle {
     private Double parA = 0.0;
     private Double parB = 0.0;
@@ -19,5 +21,26 @@ public class Rectangle {
 
     public Double calcSurface() {
         return parA*parB;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return Objects.equals(parA, rectangle.parA) && Objects.equals(parB, rectangle.parB);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(parA, parB);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "parA=" + parA +
+                ", parB=" + parB +
+                '}';
     }
 }
