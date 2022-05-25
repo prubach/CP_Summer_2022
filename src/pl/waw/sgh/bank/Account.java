82769c5b4f6b43f5afe9a3bc5706a93f9f0b778f
prubach <pawel.rubach@sgh.waw.pl>
2022-05-25 12:20:52 +0200

    Defining Bank structure: Customer, Account

---
+++ src/pl/waw/sgh/bank/Account.java
@@ -0,0 +1,59 @@
+package pl.waw.sgh.bank;
+
+import java.math.BigDecimal;
+
+public class Account {
+
+    private Integer accountID;
+
+    private BigDecimal balance = new BigDecimal(0);
+
+    private String currency = "EUR";
+
+    private Customer customer;
+
+    public Account(Integer accountID, String currency, Customer customer) {
+        this.accountID = accountID;
+        this.currency = currency;
+        this.customer = customer;
+    }
+
+    public Integer getAccountID() {
+        return accountID;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    //TODO To be deleted later
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountID=" + accountID +
+                ", balance=" + balance +
+                ", currency='" + currency + '\'' +
+                ", customer=" + customer +
+                '}';
+    }
+}
